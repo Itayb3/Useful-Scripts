4aa60eec3c95fc905e917507339d8202fe362e0b
Itay <95435881+Itayb3@users.noreply.github.com>
2023-08-20 17:27:32 +0300

    Update UserExporting.ps1

--- UserExporting.ps1
+++ UserExporting.ps1
@@ -2,6 +2,6 @@
 $ChosenOu = "fill the chosen OU here"
 $users = Get-ADUser -Filter * -SearchBase $ouDistinguishedName -Properties Enabled
 Write-Host "Total users found:" $users.Count
-$ChosenPath #where you want to save the output
+$ChosenPath = " " #where you want to save the output
 $users | Select-Object Name, Enabled | Export-Csv -Path $ChosenPath -NoTypeInformation
 Write-Host "Exported list to ChosenPath"
