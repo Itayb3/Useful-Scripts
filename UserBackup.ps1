9be05504c30005b2a7fdb59b776444ec9a4c6a48
Itay <95435881+Itayb3@users.noreply.github.com>
2023-08-20 18:13:05 +0300

    Add files via upload

---
+++ UserBackup.ps1
@@ -0,0 +1,10 @@
+﻿$newstring = (Get-WmiObject -Class win32_computerSystem | select username).username
+$CurrentUser = $newstring.Remove(0,9)
+$sourcePath = "C:\Users\${CurrentUser}\"
+$DestinationPath = " the shared folder" #REPLACE THIS WITH THE FOLDER PATH
+if (! (Test-Path -Path $DestinationPath))
+{
+New-Item -ItemType Directory -Path $DestinationPath
+}
+Copy-Item -Path $sourcePath -Destination $DestinationPath -Recurse -Force
+Write-Output "Backup created successfully!"
