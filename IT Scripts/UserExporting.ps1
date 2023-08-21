fb2286d096f8c3d239017a67e560378e6a5e2787
Itay <95435881+Itayb3@users.noreply.github.com>
2023-08-21 16:56:57 +0300

    Add files via upload

---
+++ IT Scripts/UserExporting.ps1
@@ -0,0 +1,7 @@
+﻿Import-Module ActiveDirectory
+$ChosenOu = "fill the chosen OU here"
+$users = Get-ADUser -Filter * -SearchBase $ouDistinguishedName -Properties Enabled
+Write-Host "Total users found:" $users.Count
+$ChosenPath #where you want to save the output
+$users | Select-Object Name, Enabled | Export-Csv -Path $ChosenPath -NoTypeInformation
+Write-Host "Exported list to ChosenPath"
