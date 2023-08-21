508edaf55466bff62919bdabedb8ee1a11b139e9
Itay <95435881+Itayb3@users.noreply.github.com>
2023-08-21 14:31:43 +0300

    Add files via upload

---
+++ 30DaysPasswordResets.ps1
@@ -0,0 +1,6 @@
+﻿$date = (Get-Date).AddDays(-30)
+﻿$newstring = (Get-WmiObject -Class win32_computerSystem | select username).username
+$CurrentUser = $newstring.Remove(0,9)
+$users = Get-ADUser -Filter {pwdLastSet -gt $date} -Properties pwdLastSet
+$users | Select-Object -Property Name | Export-Csv -Path "C:\Users\${CurrentUser}\Desktop\ScriptResults.csv" -NoTypeInformation
+Write-Host "Number of password resets this month:" $users.count
